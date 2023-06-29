9de3769b0a7234821fa9ab459c9a8b7b97fcbb79
GuiOliveira27 <guilherme_oliveira11@estudante.ifms.edu.br>
2023-06-29 19:44:36 -0400

    :feat: Product memory

--- src/main/java/com/solucoesageis/webproject/entities/Product.java
+++ src/main/java/com/solucoesageis/webproject/entities/Product.java
@@ -1,29 +1,27 @@
 package com.solucoesageis.webproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
-	
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
+	private Category category;
 	
-	public Category() {
+	public Product() {
 		
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -42,22 +40,25 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	
-	
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id  == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -68,7 +69,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 	
